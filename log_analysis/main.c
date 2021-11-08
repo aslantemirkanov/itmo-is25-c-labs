35c70b92536073e85057110761e5f506bed5d250
Aslan <aat0007@mail.ru>
2021-11-08 19:13:50 +0300

    lab_3 started

---
+++ log_analysis/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void errors_count(){
+    FILE *file_input;
+    file_input= fopen("access_log_Jul95.txt", "r");
+    FILE *file_output;
+    file_output= fopen("result.txt", "w");
+    int chr;
+    char str[500];
+    char *str_pointer;
+    int error_str_count=0;
+    while (1) {
+        str_pointer = fgets(str, sizeof(str), file_input);
+        //конец файла
+        if (str_pointer == NULL) {
+            if (feof(file_input) != 0) {
+                fprintf(file_output,"reading has finished\n");
+                break;
+            }
+        }
+        int str_len= strlen(str);
+        int i=str_len;
+        while(str[i]!='"' && i>=0){
+            i--;
+        }
+        if (str[i+2]=='5'){
+            error_str_count++;
+            fprintf(file_output,"%d - %s",error_str_count,str);
+        }
+    }
+    fprintf(file_output,"%d",error_str_count);
+    fclose(file_input);
+    fclose(file_output);
+    return;
+}
+
+int main() {
+    errors_count();
+
+}
